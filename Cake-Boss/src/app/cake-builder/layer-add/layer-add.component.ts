899bebafc4318f1a75c37ba56045447a517d4f13
Joshua DiChello <jddichello@gmail.com>
2024-11-11 05:43:07 -0500

    fixed circular dependency issue causing nothing to display

--- Cake-Boss/src/app/cake-builder/layer-add/layer-add.component.ts
+++ Cake-Boss/src/app/cake-builder/layer-add/layer-add.component.ts
@@ -2,13 +2,12 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { CakeBuilderComponent } from '../cake-builder.component';
 import { LayerFormComponent } from '../layer-form/layer-form.component';
 
 @Component({
   selector: 'app-layer-add',
   standalone: true,  // Mark as standalone
-  imports: [CommonModule, FormsModule, CakeBuilderComponent, LayerFormComponent],  // Import necessary modules
+  imports: [CommonModule, FormsModule, LayerFormComponent],  // Import necessary modules
   templateUrl: `./layer-add.component.html`,
   styleUrls: ['./layer-add.component.css']
 })
